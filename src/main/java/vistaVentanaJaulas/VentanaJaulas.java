445dd8ceef6e83da912b5bd1d2a7e5949a38fced
Juan Carlos <Juan Carlos@192.168.1.5>
2018-05-15 14:03:08 -0400

    Actualizacion clase VentanaJaulas

--- src/main/java/vistaVentanaJaulas/VentanaJaulas.java
+++ src/main/java/vistaVentanaJaulas/VentanaJaulas.java
@@ -5,10 +5,38 @@
  */
 package vistaVentanaJaulas;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Juan Carlos
  */
-public class VentanaJaulas {
+public class VentanaJaulas extends JFrame implements ActionListener{
+    
+    private int cantidadJaulas;
+    public ArrayList<Jaula> jaulas;
+    
+    public VentanaJaulas(){        
+        this.inicializarComponentes();
+    }
+    
+    private void inicializarComponentes(){
+        GridLayout distribucion = new GridLayout(((int)(cantidadJaulas/10)), 10);
+        this.setLayout(distribucion);
+        
+        this.jaulas = new ArrayList<>();
+        for(int i=0; i<cantidadJaulas; i++){
+            this.jaulas.add(new Jaula());            
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
