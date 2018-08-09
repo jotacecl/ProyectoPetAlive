82758b27c7bba01f3cae14bcc21d70e2dfd674f1
Juan Carlos <Juan Carlos@192.168.1.7>
2018-08-09 10:37:17 -0400

    Se pulen detalles, proyecto finalizado

--- src/main/java/vista/vistaJaulas/Jaula.java
+++ src/main/java/vista/vistaJaulas/Jaula.java
@@ -14,8 +14,7 @@ public class Jaula extends JButton{
     private boolean ocupada;
     private final String dir1 = "data/icon/MiddleCage.png";
     private final String dir2 = "data/icon/MiddleCage2.png";
-    private final String imgOcupada = "data/icon/MiddleCageOcupada.png";
-    private final String imgOcupada2 = "data/icon/MiddleCageOcupada2.png";
+    private final String imgOcupada = "data/icon/MiddleCageOcupada.png";   
     
     public Jaula(){
         Font font = new Font("Arial", Font.PLAIN, 20);        
@@ -39,7 +38,7 @@ public class Jaula extends JButton{
         if(this.isOcupada()==true){
             this.setPressedIcon(new ImageIcon(imgOcupada));
             this.setIcon(new ImageIcon(imgOcupada));
-            this.setRolloverIcon(new ImageIcon(imgOcupada2));  
+            this.setRolloverIcon(new ImageIcon(imgOcupada));  
         }else if(this.isOcupada()==false){
             this.setPressedIcon(new ImageIcon(dir1));
             this.setIcon(new ImageIcon(dir1));
